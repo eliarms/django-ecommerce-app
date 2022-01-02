bd4b05ed394b62189e99723b1a7d7e713e3eb0f9
Emmanuel Eliason-Armstrong <geteliarms@gmail.com>
2022-01-02 14:17:12 -0500

    feat(migrations): Added liked items

--- likes/models.py
+++ likes/models.py
@@ -5,7 +5,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 
 
-class TaggedItem (models.Model):
+class LikedItem (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
